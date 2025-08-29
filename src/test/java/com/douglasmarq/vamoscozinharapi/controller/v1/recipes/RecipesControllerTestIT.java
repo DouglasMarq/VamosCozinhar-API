3dfd45f5ee85b24ae1c4d98c411ec58c2fb43401
Douglas Marques Alves <douglas.marq.alves@outlook.com>
2025-08-29 00:31:02 -0300

    chore: adding empty test

---
+++ src/test/java/com/douglasmarq/vamoscozinharapi/controller/v1/recipes/RecipesControllerTestIT.java
@@ -0,0 +1,8 @@
+package com.douglasmarq.vamoscozinharapi.controller.v1.recipes;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RecipesControllerTestIT {
+}
